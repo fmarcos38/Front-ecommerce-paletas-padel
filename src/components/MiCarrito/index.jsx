1bfeb25b72c629b7062810d62876d3ae95dcb989
fmarcos38 <fmarcos_23@hotmail.com>
2025-01-27 09:27:28 -0300

    responsive carrito

--- src/components/MiCarrito/index.jsx
+++ src/components/MiCarrito/index.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getCarrito, getUsuarioById } from '../../redux/actions/actions';
+import { userData } from '../../localStorage';
 import WarningIcon from '@mui/icons-material/Warning';
 import CardProdMiCarrito from '../CardProdMiCarrito' 
 import ResumenCompra from '../ResumenCompra';
@@ -8,7 +10,10 @@ import './styles.css';
 
 function MiCarrito() {
 
+    const cliente = userData();
     const carrito = useSelector(state => state.carrito);
+    const dispatch = useDispatch();
+
 
     //función busca si hay productos sin stock
     const hayProdSinStock = () => {
@@ -25,7 +30,15 @@ function MiCarrito() {
     const handleClickVolver = () => {
         window.history.back();
     }
-    
+
+    useEffect(() => {
+            if (cliente.user.id) {
+                dispatch(getUsuarioById(cliente.user.id));
+                dispatch(getCarrito(cliente.user.id));
+            }
+            // eslint-disable-next-line react-hooks/exhaustive-deps
+        }, [dispatch]);
+
     return (
         <div className='cont-miCarrito'>
             {/* nav */}
